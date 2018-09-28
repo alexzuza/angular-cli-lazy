e847c0750f8762e2a6e24093d7dddee0d10ae4dc
Lionel Meunier <lionel.meunier@rakuten.com>
2018-09-28 14:55:52 +0200

    Migrate eample to angular 6

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {SystemJsNgModuleLoader} from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        SystemJsNgModuleLoader!
+      ]
     }).compileComponents();
   }));
 
